24e87e9724b437b0974ce8debb6463e10b957cae
johna3212 <52121529+johna3212@users.noreply.github.com>
2019-10-02 17:28:31 +0530

    Removing events from here and adding in events file

--- api.js
+++ api.js
@@ -7,28 +7,6 @@ bot.on("ready", async () => {
     bot.user.setActivity(`Ver: ${version} | Prefix: ${prefix}`, {type: "WATCHING"});
 });
 // Listener Event: Bot Launched: Check if member is added
-bot.on('guildMemberAdd', member => {
-
-
-    const members = member.guild.roles.find(`name`, "M | Member");
-
-    members.addRole(member);
-    let Wchannel = message.guild.channels.find(c => c.name === "welcome-goodbye");
-
-    let jembed = new RichEmbed()
-        .setColor("GREEN")
-        .setDescription(`${member.user.username} just joined the server`);
-
-    Wchannel.send(jembed);
-
-    let pembed = new RichEmbed()
-        .setColor("BLUE")
-        .setDescription(`Welcome to ${message.guild.name}, hope you enjoy your stay! Please read`)
-    member.send(pembed)
-
-
-    
-});
 
 ["commands", "aliases"].forEach(x => bot[x] = new Collection());
 ["console", "command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
